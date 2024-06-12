7b6ed26757ccb79ec6aa23305481d677d8460755
Sonuzz <merceralex@gmail.com>
2024-06-12 13:26:06 +0530

    Added list

--- pyPrac.py
+++ pyPrac.py
@@ -1,6 +1,16 @@
-print('Welcome to Your first program in Python')
-#name=input("name:")
-a=int(input("a="))
-b=int(input("b="))
-sum=a+b
-print("Sum =",sum)
+print('Welcome to Python Programming')
+# name=input("name:")
+# a=int(input("a="))
+# b=int(input("b="))
+# sum=a+b
+# print("Sum =",sum)
+
+## List 
+list=[1,2,45,56,"Me"] #Syntax of list
+print(list[0])
+list.append(4)
+print(list)
+#There are lot other Methods check notes or python Docs for more
+
+## Tuple
+
