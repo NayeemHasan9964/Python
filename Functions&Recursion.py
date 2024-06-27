b2f4f1d1ff264b9be624010f66c5a56ce495ec27
Sonuzz <merceralex@gmail.com>
2024-06-27 22:30:44 +0530

    Added Recursion

--- Functions&Recursion.py
+++ Functions&Recursion.py
@@ -26,13 +26,45 @@
 
 
 # WAF to print the factorial of n(n is the parameter)
-def factorial_Of_n(n):
-    fact=1
-    if n==0:
-        print("Cannot start with Zero")
-    for i in range(1,n+1):
-        fact*=i
-    return fact
+# def factorial_Of_n(n):
+#     fact=1
+#     if n==0:
+#         print("Cannot start with Zero")
+#     for i in range(1,n+1):
+#         fact*=i
+#     return fact
     
-print(factorial_Of_n(5))
+# print(factorial_Of_n(5))
+
+# Recursion-->When a function calls its self again is called "Recursion"
+# Its similar to For and while loops
+
+def RecursionExample(n):
+    if (n==0): # Base case
+        return 
+    print(n)
+    RecursionExample(n-1)
+RecursionExample(5)
+
+# WAP recurison program to calculate sum of N natural numbers
+
+def cal_n_Natural_Numbers(n):
+    if n==0:
+        return 0
+    else:
+        return n+cal_n_Natural_Numbers(n-1)
+
+print(cal_n_Natural_Numbers(10))    
+
+#WAP using recursion to print elements in the list
+
+def printElements(list,idx):
+    if idx==len(list):
+        return
+    print(list[idx])
+    printElements(list,idx+1)
+    
+printElements([1,2,3,4,5],0)
+    
+
       
