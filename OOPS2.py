8040f75883f71a221fe4ad6e81aaaa5e9030e060
Sonuzz <merceralex@gmail.com>
2024-07-09 21:57:54 +0530

    Added notes

--- OOPS2.py
+++ OOPS2.py
@@ -8,7 +8,23 @@ class Student:
 class Account:
     def __init__(self,owner,balance=0):
         self.__owner=owner # private Attribute
+        self.__balance=balance
+        
+    def setOwner(self,owner):
+        self.__owner=owner
+        
+    def setBalance(self,balance):
+        self.__balance=balance
+    
+    def printDetails(self):
+        print("Owner:",self.__owner)
+        print("Balance:",self.__balance)
+    
+    
         
 
-Acc1=Account("hulk", 0)
-print(Acc1.__owner)        
+Acc1=Account("enemy", 2344)
+print(Acc1.printDetails())
+
+
+# Inheritance      
