dbb269f127e623529cc963e0ed44d15bbe724039
Sonuzz <merceralex@gmail.com>
2024-07-16 22:57:41 +0530

    solved another problem

--- pythonQuestions.py
+++ pythonQuestions.py
@@ -19,7 +19,20 @@
 # print(sum_of_digits(12345))  
 
 # function to save time
-def time_saved(distance, speed1, speed2):
-    print(distance/speed1- distance/speed2)
-    return round(distance/speed1- distance/speed2,2)
-print(time_saved(200,60,80))   
+# def time_saved(distance, speed1, speed2):
+#     print(distance/speed1- distance/speed2)
+#     return round(distance/speed1- distance/speed2,2)
+# print(time_saved(200,60,80)) 
+
+# Find the index of the first vowel in the String
+def first_vowel_index(s):
+    x = s.lower()
+    z = list(x)
+    y = ['a','e','i','o','u']
+    for i in range(0 ,len(x)):
+        for j in range(0 , len(y)):
+            if x[i] == y[j]:
+                return i
+print(first_vowel_index('PYTHON'))
+
+  
