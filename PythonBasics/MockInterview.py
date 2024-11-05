c27a19418ed2e144f007507c022cdab440a76966
Sonuzz <merceralex@gmail.com>
2024-11-05 13:33:38 +0530

    Added questions in Mock interview

--- PythonBasics/MockInterview.py
+++ PythonBasics/MockInterview.py
@@ -3,22 +3,18 @@
 #     reversed_string = lower_case[::-1]
 #     return reversed_string
 # print(Reverse_String("MADAM123"))
-#
-# # Write today is sunday
+
+# Write today is sunday
 # def reverse_words(String):
 #     return " ".join(String.split()[::-1])
 #
 # print(reverse_words("today is sunday"))
 
-
-# Function to check if a number is prime
-# Python program to display all the prime numbers within an interval
-
+# # Function to check if a number is prime
+# # Python program to display all the prime numbers within an interval
 # lower = 2
 # upper = 100
-#
 # print("Prime numbers between", lower, "and", upper, "are:")
-#
 # for num in range(lower, upper + 1):
 #    # all prime numbers are greater than 1
 #    if num > 1:
@@ -27,10 +23,27 @@
 #                break
 #        else:
 #            print(num)
+#
+# list1 = [99,88,77,66,55,44,33,22,11]
+# for i in range(len(list1)):
+#     for j in range(i+1,len(list1)):
+#         if list1[i]>list1[j]:
+#             list1[i],list1[j] = list1[j],list1[i]
+# print(list1)
 
-list1 = [99,88,77,66,55,44,33,22,11]
-for i in range(len(list1)):
-    for j in range(i+1,len(list1)):
-        if list1[i]>list1[j]:
-            list1[i],list1[j] = list1[j],list1[i]
-print(list1)
+# def reverse_string(s):
+#     reversed_str = ""
+#     for char in s:
+#         reversed_str = char + reversed_str  # Prepend each character to the result
+#     return reversed_str
+#
+# # Test
+# input_str = "Hello"
+# print(reverse_string(input_str))  # Output: "olleH"
+
+def sum_of_squares(numbers):
+    sum = 0
+    for i in range(len(numbers)):
+        sum = sum + numbers[i]**2
+    return sum
+print(sum_of_squares([1,2,4]))
