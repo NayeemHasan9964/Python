914d910312e3af15ea6b84eccd10ac601f5372ce
Sonuzz <merceralex@gmail.com>
2024-11-06 16:02:12 +0530

    Fibbinocci series

--- PythonBasics/MockInterview.py
+++ PythonBasics/MockInterview.py
@@ -64,3 +64,18 @@
 #             max_of_list  = lst[i]
 #     return max_of_list
 # print(find_maxNum([55,66,99,88,77,44]))
+
+#0,1,1,2,3,5...
+n = 10
+num1 = 0
+num2 = 1
+next_number = num2
+count = 1
+
+while count <= n:
+    print(next_number, end=" ")
+    count += 1
+    num1, num2 = num2, next_number
+    next_number = num1 + num2
+
+
