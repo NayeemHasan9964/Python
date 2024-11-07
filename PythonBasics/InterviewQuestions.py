1db7998682ecb3b7b731a45ed0de48f5b0fab52d
Sonuzz <merceralex@gmail.com>
2024-11-07 22:04:27 +0530

    Added list questions

--- PythonBasics/InterviewQuestions.py
+++ PythonBasics/InterviewQuestions.py
@@ -59,12 +59,81 @@
 #                 print(num)
 # to_PrimeInRange()
 # Write a function to find the sum of digits of a given number.
-def sumOfDigits(num):
-    sum = 0
-    while num > 0:
-        sum = sum + num%10
-        num = num//10
-    return sum
-print(sumOfDigits(420))
+# def sumOfDigits(num):
+#     sum = 0
+#     while num > 0:
+#         sum = sum + num%10
+#         num = num//10
+#     return sum
+# print(sumOfDigits(420))
+# Write a function to Reverse a number
+# def rev_num(num):
+#     rev = 0
+#     while num>0:
+#         n = num%10
+#         rev = rev*10 + n
+#         num = num//10
+#     return rev
+# print(rev_num(1234))
+from PythonBasics.Files import new_data
 
 
+# 3. Working with Lists
+# Write a function to find the second largest element in a list.
+# def to_findSecondLargest(lst):
+#     max_num = lst[0]
+#     for i in range(0, len(lst)):
+#         if lst[i] > max_num:
+#             max_num = lst[i]
+#     lst.remove(max_num)
+#     new_lst = lst
+#     second_largest = new_lst[0]
+#     for j in range(0, len(new_lst)):
+#         if new_lst[j] > second_largest:
+#             second_largest = new_lst[j]
+#     return second_largest
+# print(to_findSecondLargest([13,23,11,45,38,68]))
+
+
+# Write a function to find the maximum and minimum in a list without using built-in functions.
+# def max_min_numInList(lst):
+#     max_num = lst[0]
+#     min_num = lst[0]
+#     for i in range(0,len(lst)):
+#         if lst[i]>max_num:
+#             max_num = lst[i]
+#         elif lst[i]<min_num:
+#             min_num = lst[i]
+#     return max_num,min_num
+# print(max_min_numInList([13,23,11,45,38,68]))
+# Write a function to remove duplicates from a list.
+# def to_removeDuplicates(lst):
+#     i = 0
+#     while i < len(lst):
+#         j = i + 1
+#         while j < len(lst):
+#             if lst[i] == lst[j]:
+#                 del lst[j]  # Remove the duplicate element at index j
+#             else:
+#                 j += 1  # Move to the next element only if no deletion occurred
+#         i += 1  # Move to the next element in the outer loop
+#     return lst
+# print(to_removeDuplicates([13,23,11,45,38,11,68]))  # Output should be [1, 2, 3, 4, 5]
+
+
+# Write a function to sort a list in descending order without using built-in sort functions
+# def to_sortInDescending(lst):
+#     for i in range(0,len(lst)):
+#         for j in range(i+1,len(lst)):
+#             if lst[i]<lst[j]:
+#                 lst[i],lst[j]=lst[j],lst[i]
+#     return lst
+# print(to_sortInDescending([11,88,77,66,55,44,33,22,99]))
+#
+# def to_sortInAscending(lst):
+#     for i in range(0,len(lst)):
+#         for j in range(i+1,len(lst)):
+#             if lst[i]>lst[j]:
+#                 lst[i],lst[j]=lst[j],lst[i]
+#     return lst
+# print(to_sortInAscending([11,88,77,66,55,44,33,22,99]))
--- PythonBasics/MockInterview.py
+++ PythonBasics/MockInterview.py
@@ -66,16 +66,16 @@
 # print(find_maxNum([55,66,99,88,77,44]))
 
 #0,1,1,2,3,5...
-n = 10
-num1 = 0
-num2 = 1
-next_number = num2
-count = 1
-
-while count <= n:
-    print(next_number, end=" ")
-    count += 1
-    num1, num2 = num2, next_number
-    next_number = num1 + num2
+# n = 10
+# num1 = 0
+# num2 = 1
+# next_number = num2
+# count = 1
+#
+# while count <= n:
+#     print(next_number, end=" ")
+#     count += 1
+#     num1, num2 = num2, next_number
+#     next_number = num1 + num2
 
 
