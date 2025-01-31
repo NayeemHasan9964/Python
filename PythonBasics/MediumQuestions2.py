52667ff179ad56c082c9ca465fb886b7238a3c0c
Sonuzz <merceralex@gmail.com>
2025-01-31 17:37:11 +0530

    Added questions

--- PythonBasics/MediumQuestions2.py
+++ PythonBasics/MediumQuestions2.py
@@ -89,3 +89,79 @@
 #     return f"{sentence.strip()}"
 #
 # print(correct_sentence('this is a test'))
+
+#Total time spend on Washing Hands
+# def total_washing_time(times_per_day, months):
+#     oneHandWashInSeconds = 21
+#     totalSecondsInADay = oneHandWashInSeconds * times_per_day
+#     convertingIntoMinutes = totalSecondsInADay/60
+#     totalTimeInMonths = (convertingIntoMinutes * 30) * months
+#     totalMinutes = int(totalTimeInMonths)
+#     totalSeconds = round((totalTimeInMonths - totalMinutes) * 60)
+#     return f"{totalMinutes} minutes and {totalSeconds} seconds"
+# print(total_washing_time(5,3))
+
+# Check value is in Dict or not
+# def value_exist(dict1, val):
+#     for values in dict1:
+#         if dict1[values]==val:
+#             return True
+#     return False
+# print(value_exist({'a':1,'b':2},1))
+
+# Triple Function
+# def triple_function(a,b,c):
+#     if a == True and b == True and c == True:
+#         return True
+#     else:
+#         return False
+# print(triple_function(True,False,True))
+
+# String can be obtained from another string
+# def is_long_pressed(name, typed):
+#     name_list = list(name)
+#     name_typed = list(typed)
+#     for i in range(0, len(typed)):
+#         if name_list[i] == name_typed[i]:
+#             continue
+#         else:
+#             name_list.insert(i,name_typed[i])
+#     return name_list==name_typed
+# print(is_long_pressed("alex","aaleex"))
+
+# Pronounce of X,s
+# def pronounce_xs(s):
+#     vowels = 'aeiouAEIOU'
+#     result = []
+#     i = 0
+#
+#     while i < len(s):
+#         if s[i] == 'x':
+#             if i + 1 < len(s) and s[i + 1] in vowels:
+#                 result.append("cks")
+#             else:
+#                 result.append("k")
+#         else:
+#             result.append(s[i])
+#         i += 1
+#
+#     return ''.join(result)
+# print(pronounce_xs("xerox"))
+
+# convert a color from CMYK format to RGB format
+# def cmyk_to_rgb(cmyk):
+#     c,m,y,k =  cmyk
+#     R = 255 * (1-c)*(1-k)
+#     G = 255 * (1-m)*(1-k)
+#     B = 255 * (1-y)*(1-k)
+#     return round(R),round(G),round(B)
+# print(cmyk_to_rgb([0.4,0.49,0.552,0.36]))
+
+# heterogeneous Dict
+def filter_dict(d):
+    result = {key: value for key, value in d.items() if isinstance(value, int)}
+    return result
+print(filter_dict({'a':1,'b':'2','c':3.0,'d':[4],'e':5}))
+
+
+
